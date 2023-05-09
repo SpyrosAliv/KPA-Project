b640d6a8c03b391c2da39692bec452bc709ebe0a
SpyrosAliv <spyrosalivizatos8@gmail.com>
2023-05-09 20:50:29 +0300

    Updated Interface

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const mainTitleDisplay = document.querySelector("#starting-title");
 const infoDisplay = document.querySelector("#info");
+function clear() {
+    mainTitleDisplay.outerHTML = "";
+    infoDisplay.hidden = true;
+}
 window.onload = () => {
     infoDisplay.hidden = true;
 }
 
 function loadGeneralInfo() {
+    clear();
     infoDisplay.hidden = false;
-    mainTitleDisplay.outerHTML = "";
 }
