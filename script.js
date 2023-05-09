e2e1cc1f04fee639bff316a8628b5c8d572b6658
SpyrosAliv <spyrosalivizatos8@gmail.com>
2023-05-09 22:00:28 +0300

    Filled description tab, minor changes to interface

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const mainTitleDisplay = document.querySelector("#starting-title");
 const infoDisplay = document.querySelector("#info");
+const plotDesc = document.querySelector("#plot-description");
+let hasDisappeared = false;
 function clear() {
-    mainTitleDisplay.outerHTML = "";
+    if (!hasDisappeared) {
+        mainTitleDisplay.outerHTML = "";
+    }
     infoDisplay.hidden = true;
+    plotDesc.hidden = true;
+    hasDisappeared = true;
 }
 window.onload = () => {
     infoDisplay.hidden = true;
+    plotDesc.hidden = true;
 }
 
 function loadGeneralInfo() {
     clear();
     infoDisplay.hidden = false;
+}
+
+function loadPlotDescription() {
+    clear();
+    plotDesc.hidden = false;
 }
