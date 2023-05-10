5c164c510ce652e249891a0d9e6c6e37dcfc6b31
SpyrosAliv <spyrosalivizatos8@gmail.com>
2023-05-10 15:49:57 +0300

    finalized final tab

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const mainTitleDisplay = document.querySelector("#starting-title");
 const infoDisplay = document.querySelector("#info");
 const plotDesc = document.querySelector("#plot-description");
+const finalDisplay = document.querySelector("#finale");
 let hasDisappeared = false;
+
 function clear() {
     if (!hasDisappeared) {
         mainTitleDisplay.outerHTML = "";
     }
+    finalDisplay.hidden=true;
     infoDisplay.hidden = true;
     plotDesc.hidden = true;
     hasDisappeared = true;
 }
 window.onload = () => {
+    finalDisplay.hidden=true;
     infoDisplay.hidden = true;
     plotDesc.hidden = true;
 }
@@ -23,4 +27,9 @@ function loadGeneralInfo() {
 function loadPlotDescription() {
     clear();
     plotDesc.hidden = false;
+}
+
+function loadFinaleInfo() {
+    clear();
+    finalDisplay.hidden = false;
 }
