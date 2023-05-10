fc2158a0fbf717581d6aa0b84f379b4f50cab2e7
SpyrosAliv <spyrosalivizatos8@gmail.com>
2023-05-10 19:02:43 +0300

    film options, interface updates

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-const mainTitleDisplay = document.querySelector("#starting-title");
 const infoDisplay = document.querySelector("#info");
 const plotDesc = document.querySelector("#plot-description");
 const finalDisplay = document.querySelector("#finale");
-let hasDisappeared = false;
-
-function clear() {
-    if (!hasDisappeared) {
-        mainTitleDisplay.outerHTML = "";
-    }
+const spaceOdyssey = document.querySelector("#space-odyssey");
+const spaceOdysseyMenu = document.querySelector("#menu");
+function clearSpaceOdyssey() {
     finalDisplay.hidden=true;
     infoDisplay.hidden = true;
     plotDesc.hidden = true;
-    hasDisappeared = true;
 }
+
+function callSpaceOdyssey() {
+    spaceOdyssey.hidden = false;
+    loadGeneralInfo();
+    spaceOdysseyMenu.scrollIntoView();
+}
+
 window.onload = () => {
     finalDisplay.hidden=true;
     infoDisplay.hidden = true;
     plotDesc.hidden = true;
+    spaceOdyssey.hidden = true;
 }
 
 function loadGeneralInfo() {
-    clear();
+    clearSpaceOdyssey();
     infoDisplay.hidden = false;
 }
 
 function loadPlotDescription() {
-    clear();
+    clearSpaceOdyssey();
     plotDesc.hidden = false;
 }
 
 function loadFinaleInfo() {
-    clear();
+    clearSpaceOdyssey();
     finalDisplay.hidden = false;
 }
